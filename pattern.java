3221b8e890567e5a77afb6d69cdee57e114bc74c
Faziha Tabassum <156168807+zeehawww@users.noreply.github.com>
2025-12-13 10:03:42 +0530

    Create pattern.java
    printing a pattern in java

---
+++ pattern.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class coder {  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=num;i>=1;i--){
+            System.out.print("#");
+        }
+        sc.close();
+    }
+}
