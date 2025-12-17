8d9640264496727c7ef805d02945b8208f7297b9
Faziha Tabassum <156168807+zeehawww@users.noreply.github.com>
2025-12-17 20:46:14 +0530

    Create uppercase.java

---
+++ uppercase.java
@@ -0,0 +1,11 @@
+
+class coding{
+    public static void main(String[] args){
+
+        String lowercaseString = "faziha";
+        String uppercaseString = lowercaseString.toUpperCase();
+        System.out.println("Lowercase String:" + lowercaseString);
+        System.out.println("Uppercase String: " + uppercaseString);
+
+    }
+}
